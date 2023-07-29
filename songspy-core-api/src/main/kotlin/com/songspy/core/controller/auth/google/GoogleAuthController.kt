8098f3ea1abbe7b0e48760357d33d260a5e4157c
blackSooooo <blacksooooo@naver.com>
2023-07-29 21:45:37 +0900

    change http method in google auth

--- songspy-core-api/src/main/kotlin/com/songspy/core/controller/auth/google/GoogleAuthController.kt
+++ songspy-core-api/src/main/kotlin/com/songspy/core/controller/auth/google/GoogleAuthController.kt
@@ -3,7 +3,7 @@ package com.songspy.core.controller.auth.google
 import com.songspy.commons.api.response.CommonApiResponse
 import com.songspy.core.controller.auth.google.response.AuthResponse
 import com.songspy.core.domains.auth.google.GoogleAuthService
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController
 class GoogleAuthController(
     private val googleAuthService: GoogleAuthService
 ) {
-    @GetMapping
+    @PostMapping
     fun auth(
         @RequestParam code: String
     ): CommonApiResponse<AuthResponse> {
