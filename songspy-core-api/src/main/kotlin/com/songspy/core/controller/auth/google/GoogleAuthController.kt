e28ccdb0d5ab75d9ab6c54407bf5fc54914b6fd9
blackSooooo <blacksooooo@naver.com>
2023-07-29 21:56:39 +0900

    add songspy in api path

--- songspy-core-api/src/main/kotlin/com/songspy/core/controller/auth/google/GoogleAuthController.kt
+++ songspy-core-api/src/main/kotlin/com/songspy/core/controller/auth/google/GoogleAuthController.kt
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/v1/google-auth")
+@RequestMapping("/v1/songspy/google-auth")
 class GoogleAuthController(
     private val googleAuthService: GoogleAuthService
 ) {
--- songspy-core-api/src/main/kotlin/com/songspy/core/controller/auth/spotify/SpotifyAuthController.kt
+++ songspy-core-api/src/main/kotlin/com/songspy/core/controller/auth/spotify/SpotifyAuthController.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/v1/spotify-auth")
+@RequestMapping("/v1/songspy/spotify-auth")
 class SpotifyAuthController(
     private val spotifyAuthService: SpotifyAuthService
 ) {
