c43e18c0c6840af020f1e609f92126a640231aea
blackSooooo <blacksooooo@naver.com>
2023-07-29 17:04:19 +0900

    add refresh token api in spotify oauth client

--- clients/spotify/src/main/kotlin/com/songspy/clients/spotify/oauth/SpotifyOAuthApi.kt
+++ clients/spotify/src/main/kotlin/com/songspy/clients/spotify/oauth/SpotifyOAuthApi.kt
@@ -1,6 +1,7 @@
 package com.songspy.clients.spotify.oauth
 
-import com.songspy.clients.spotify.response.AccessTokenResponse
+import com.songspy.clients.spotify.response.AccessTokenResponseDto
+import com.songspy.clients.spotify.response.TokenRefreshResponseDto
 import com.songspy.commons.extension.BasicAuth
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.http.MediaType
@@ -20,5 +21,16 @@ interface SpotifyOAuthApi {
         @RequestParam("grant_type") grantType: String = "authorization_code",
         @RequestParam("code") code: String,
         @RequestParam("redirect_uri") redirectUrl: String
-    ): AccessTokenResponse
+    ): AccessTokenResponseDto
+
+    @PostMapping(
+        value = ["/api/token"],
+        consumes = [MediaType.APPLICATION_FORM_URLENCODED_VALUE],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    fun refresh(
+        @RequestHeader("Authorization") auth: BasicAuth,
+        @RequestParam("grant_type") grantType: String = "refresh_token",
+        @RequestParam("refresh_token") refreshToken: String
+    ): TokenRefreshResponseDto
 }
