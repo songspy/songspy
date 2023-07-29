0e70f309bbac693fa831452c2d7445bd10e3fe17
blackSooooo <blacksooooo@naver.com>
2023-07-29 21:09:50 +0900

    add jwt handler

--- songspy-core-api/build.gradle.kts
+++ songspy-core-api/build.gradle.kts
@@ -1,12 +1,22 @@
 val mockkVersion: String by project
 val kotestVersion: String by project
+val jwtVersion: String by project
 
 dependencies {
+    implementation(project(":commons:api"))
+
     implementation(project(":clients:google"))
+
     implementation(project(":storage:db-core"))
+
     implementation(project(":songspy-batch"))
 
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    implementation("io.jsonwebtoken:jjwt-api:$jwtVersion")
+    implementation("io.jsonwebtoken:jjwt-impl:$jwtVersion")
+    implementation("io.jsonwebtoken:jjwt-jackson:$jwtVersion")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.mockk:mockk:$mockkVersion")
