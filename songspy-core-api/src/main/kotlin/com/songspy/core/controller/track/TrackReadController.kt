dda06cc6f75fb7e79cae94fbb06d67587b845e82
blackSooooo <blacksooooo@naver.com>
2023-07-29 22:22:29 +0900

    add currently-playing-track api

---
+++ songspy-core-api/src/main/kotlin/com/songspy/core/controller/track/TrackReadController.kt
@@ -0,0 +1,21 @@
+package com.songspy.core.controller.track
+
+import com.songspy.commons.api.response.CommonApiResponse
+import com.songspy.core.domains.track.TrackReadService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/v1/songspy/tracks")
+class TrackReadController(
+    private val trackReadService: TrackReadService
+) {
+    @GetMapping("/current-playing")
+    fun getCurrentPlaying(): CommonApiResponse<List<CurrentPlayingTrackResponseDto>> {
+        val tracks = trackReadService.readCurrentPlaying()
+        return CommonApiResponse.success(
+            data = tracks.map { CurrentPlayingTrackResponseDto(it) }
+        )
+    }
+}
