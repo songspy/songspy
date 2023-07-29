0067f04aab606be4a0b98da78a6a636605de1afd
blackSooooo <blacksooooo@naver.com>
2023-07-29 15:04:57 +0900

    change return to runCatching

--- clients/spotify/src/main/kotlin/com/songspy/clients/spotify/oauth/SpotifyOAuthClient.kt
+++ clients/spotify/src/main/kotlin/com/songspy/clients/spotify/oauth/SpotifyOAuthClient.kt
@@ -1,6 +1,8 @@
 package com.songspy.clients.spotify.oauth
 
+import com.songspy.clients.spotify.response.AccessTokenResponse
 import com.songspy.commons.extension.basicAuth
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
@@ -14,12 +16,16 @@ class SpotifyOAuthClient(
     @Value("\${spotify-api.redirect.url}")
     private val redirectUrl: String
 ) {
-    fun getAccessToken(code: String) {
-        val tokenResult = spotifyOAuthApi.getAccessToken(
-            auth = basicAuth(clientKey, secretKey),
-            code = code,
-            redirectUrl = redirectUrl
-        )
-        println(tokenResult)
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    fun getAccessToken(code: String): Result<AccessTokenResponse> {
+        logger.info("[access token] code: $code")
+        return runCatching {
+            spotifyOAuthApi.getAccessToken(
+                auth = basicAuth(clientKey, secretKey),
+                code = code,
+                redirectUrl = redirectUrl
+            )
+        }
     }
 }
