576b37e9067dff5990e1575b1c19dff2831b5005
blackSooooo <blacksooooo@naver.com>
2023-07-29 15:35:59 +0900

    import storage module in core api

--- songspy-core-api/build.gradle.kts
+++ songspy-core-api/build.gradle.kts
@@ -2,6 +2,8 @@ val mockkVersion: String by project
 val kotestVersion: String by project
 
 dependencies {
+    implementation(project(":storage:db-core"))
+
     implementation("org.springframework.boot:spring-boot-starter-web")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
