aa5692596b08f04800766dc871db4586acfd09da
blackSooooo <blacksooooo@naver.com>
2023-07-29 17:06:09 +0900

    add SpotifyTokenReader component

---
+++ songspy-batch/src/main/kotlin/com/songspy/core/batch/spotify/token/SpotifyTokenReader.kt
@@ -0,0 +1,16 @@
+package com.songspy.core.batch.spotify.token
+
+import com.songspy.storage.db.core.spotify.token.TokenRepository
+import org.springframework.stereotype.Component
+import java.time.LocalDateTime
+
+@Component
+class SpotifyTokenReader(
+    private val tokenRepository: TokenRepository,
+    private val tokenBucketMapper: TokenBucketMapper
+) {
+    fun readExpired(): List<TokenBucket> {
+        val tokens = tokenRepository.findAllByExpiredAtIsLessThan(LocalDateTime.now())
+        return tokens.map { tokenBucketMapper.map(it) }
+    }
+}
