d512c5aa23dfac26df159bb2e89dcaa72fb36878
blackSooooo <blacksooooo@naver.com>
2023-07-29 21:08:11 +0900

    add user entity, repository

---
+++ storage/db-core/src/main/kotlin/com/songspy/storage/db/core/user/UserRepository.kt
@@ -0,0 +1,7 @@
+package com.songspy.storage.db.core.user
+
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface UserRepository : JpaRepository<UserEntity, Long> {
+    fun findByThirdPartyId(thirdPartyId: String): UserEntity?
+}
