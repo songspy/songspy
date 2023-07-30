62ca50d1c2c56844a3c53020f57b94168acc81e5
blackSooooo <blacksooooo@naver.com>
2023-07-30 15:28:03 +0900

    add mysql connector

--- storage/db-core/build.gradle.kts
+++ storage/db-core/build.gradle.kts
@@ -6,5 +6,6 @@ allOpen {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("com.mysql:mysql-connector-j")
     runtimeOnly("com.h2database:h2")
 }
