14c5815b173a03aa8a424ee26cbbbb697e648e4d
blackSooooo <blacksooooo@naver.com>
2023-07-30 22:06:41 +0900

    add logging in BearerAuthorizationInterceptor

--- songspy-core-api/src/main/kotlin/com/songspy/core/configuration/interceptor/BearerAuthorizationInterceptor.kt
+++ songspy-core-api/src/main/kotlin/com/songspy/core/configuration/interceptor/BearerAuthorizationInterceptor.kt
@@ -4,6 +4,7 @@ import com.songspy.core.domains.auth.JwtTokenHandler
 import com.songspy.core.support.Constants
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import org.springframework.web.servlet.HandlerInterceptor
 
@@ -11,10 +12,12 @@ import org.springframework.web.servlet.HandlerInterceptor
 class BearerAuthorizationInterceptor(
     private val jwtTokenHandler: JwtTokenHandler
 ) : HandlerInterceptor {
+    private val logger = LoggerFactory.getLogger(javaClass)
 
     override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
         val (_, payload) = request.getHeader("Authorization").split(" ")
         val userId = jwtTokenHandler.decode(payload)[Constants.USER_ID]
+        logger.info("BearerAuthorization userId resolve: $userId")
         request.setAttribute(Constants.USER_ID, userId)
         return true
     }
