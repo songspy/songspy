eedf87bd33db55a674f0d892a21d68ee3c695b3b
blackSooooo <blacksooooo@naver.com>
2023-08-15 22:57:26 +0900

    fix scheduling time

--- songspy-batch/src/main/kotlin/com/songspy/core/batch/spotify/track/CurrentPlayingTrackScheduler.kt
+++ songspy-batch/src/main/kotlin/com/songspy/core/batch/spotify/track/CurrentPlayingTrackScheduler.kt
@@ -46,6 +46,6 @@ class CurrentPlayingTrackScheduler(
     }
 
     companion object {
-        private const val CURRENTLY_PLAYING_TRACK__SCHEDULE_TIME = 1_000L * 30  // 30초
+        private const val CURRENTLY_PLAYING_TRACK__SCHEDULE_TIME = 1_000L * 30 // 30초
     }
 }
