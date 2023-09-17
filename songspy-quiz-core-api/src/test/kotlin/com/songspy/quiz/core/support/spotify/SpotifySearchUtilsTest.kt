247e52aef0f92c7ae73fbe24a90fc2ece5a3a244
blackSooooo <blacksooooo@naver.com>
2023-09-17 17:15:05 +0900

    add SpotifySearchUtils

---
+++ songspy-quiz-core-api/src/test/kotlin/com/songspy/quiz/core/support/spotify/SpotifySearchUtilsTest.kt
@@ -0,0 +1,10 @@
+package com.songspy.quiz.core.support.spotify
+
+import org.junit.jupiter.api.Test
+
+internal class SpotifySearchUtilsTest {
+    @Test
+    fun `href로부터 trackId를 추출한다`() {
+
+    }
+}
