66d9e7be14081be123afb5b731bd48ef87ece79a
blackSooooo <blacksooooo@naver.com>
2023-09-17 21:42:08 +0900

    add SpotifyTrackFetcher and refactor

--- songspy-quiz-core-api/src/main/kotlin/com/songspy/quiz/core/domains/playlist/TrackService.kt
+++ songspy-quiz-core-api/src/main/kotlin/com/songspy/quiz/core/domains/playlist/TrackService.kt
@@ -1,19 +1,12 @@
 package com.songspy.quiz.core.domains.playlist
 
-import com.songspy.clients.spotify.SpotifyClient
 import org.springframework.stereotype.Service
 
 @Service
 class TrackService(
-    private val spotifyClient: SpotifyClient,
-    private val playlistTrackResultMapper: PlaylistTrackResultMapper
+    private val trackFetcher: SpotifyTrackFetcher
 ) {
     fun getByPlaylistId(playlistId: String, offset: Int): PlaylistTrackResult {
-        // TODO token change (일단 mocking)
-        val spotifyToken = "token"
-        val playlistTrackResponse = spotifyClient.getPlaylistTracks(token = spotifyToken, playlistId = playlistId, offset = offset)
-            .onFailure { }
-            .getOrThrow()
-        return playlistTrackResultMapper.map(playlistTrackResponse)
+        return trackFetcher.fetchByPlaylistIdAndOffset(playlistId, offset)
     }
 }
