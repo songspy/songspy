2d2e0ba41fe74466cd15f37043cd57de97571a7a
blackSooooo <blacksooooo@naver.com>
2023-09-17 20:03:21 +0900

    add message field in CoreApiException

--- commons/exceptions/src/main/kotlin/com/songspy/commons/exceptions/ErrorType.kt
+++ commons/exceptions/src/main/kotlin/com/songspy/commons/exceptions/ErrorType.kt
@@ -3,4 +3,5 @@ package com.songspy.commons.exceptions
 import org.springframework.http.HttpStatus
 
 enum class ErrorType(val statusCode: HttpStatus, val message: String) {
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다.")
 }
