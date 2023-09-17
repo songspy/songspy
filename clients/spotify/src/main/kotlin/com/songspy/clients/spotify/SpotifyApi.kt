26508e5db6bfbefdc6b80d4bb8adabdbe3266304
blackSooooo <blacksooooo@naver.com>
2023-09-17 17:44:07 +0900

    add spotify get playlist tracks api

--- clients/spotify/src/main/kotlin/com/songspy/clients/spotify/SpotifyApi.kt
+++ clients/spotify/src/main/kotlin/com/songspy/clients/spotify/SpotifyApi.kt
@@ -1,10 +1,12 @@
 package com.songspy.clients.spotify
 
 import com.songspy.clients.spotify.response.PlayingTrackResponseDto
+import com.songspy.clients.spotify.response.SpotifyPlaylistTrackResponseDto
 import com.songspy.clients.spotify.response.SpotifySearchResponseDto
 import com.songspy.commons.extension.BearerAuth
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestParam
 
@@ -22,4 +24,14 @@ interface SpotifyApi {
         @RequestParam limit: Int = 20,
         @RequestParam offset: Int
     ): SpotifySearchResponseDto
+
+    @GetMapping("/v1/playlists/{playlist_id}/tracks")
+    fun getPlaylistTracks(
+        @RequestHeader("Authorization") auth: BearerAuth,
+        @PathVariable("playlist_id") playlistId: String,
+        @RequestParam market: String = "KR",
+        @RequestParam fields: String = "next,total,items(track.id, track.album.images, track.artists.name, track.explicit, track.name, track.preview_url)",
+        @RequestParam limit: Int = 20,
+        @RequestParam offset: Int
+    ): SpotifyPlaylistTrackResponseDto
 }
--- clients/spotify/src/main/kotlin/com/songspy/clients/spotify/SpotifyClient.kt
+++ clients/spotify/src/main/kotlin/com/songspy/clients/spotify/SpotifyClient.kt
@@ -1,6 +1,7 @@
 package com.songspy.clients.spotify
 
 import com.songspy.clients.spotify.response.PlayingTrackResponseDto
+import com.songspy.clients.spotify.response.SpotifyPlaylistTrackResponseDto
 import com.songspy.clients.spotify.response.SpotifySearchResponseDto
 import com.songspy.commons.extension.bearerAuth
 import org.slf4j.LoggerFactory
@@ -26,6 +27,16 @@ class SpotifyClient(
             spotifyApi.search(
                 auth = bearerAuth(token),
                 keyword = keyword,
+                offset = offset
+            )
+        }
+    }
+
+    fun getPlaylistTracks(token: String, playlistId: String, offset: Int): Result<SpotifyPlaylistTrackResponseDto> {
+        return runCatching {
+            spotifyApi.getPlaylistTracks(
+                auth = bearerAuth(token),
+                playlistId = playlistId,
                 offset = offset
             )
         }
